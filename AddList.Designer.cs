fcca070eb7f00c285213743cec1ebbbbf2f5fa08
Mousa Abdulaziz <mousa.abdulaziz@nrcan-rncan.gc.ca>
2023-06-05 13:47:29 -0400

    changed added list

--- AddList.Designer.cs
+++ AddList.Designer.cs
@@ -28,40 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.addFormTemplate = new System.Windows.Forms.CheckedListBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.saveButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.Content = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // panel1
+            // dataGridView1
             // 
-            this.panel1.Controls.Add(this.addFormTemplate);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(813, 374);
-            this.panel1.TabIndex = 0;
-            // 
-            // addFormTemplate
-            // 
-            this.addFormTemplate.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.addFormTemplate.FormattingEnabled = true;
-            this.addFormTemplate.Location = new System.Drawing.Point(8, 5);
-            this.addFormTemplate.Margin = new System.Windows.Forms.Padding(2);
-            this.addFormTemplate.Name = "addFormTemplate";
-            this.addFormTemplate.Size = new System.Drawing.Size(797, 364);
-            this.addFormTemplate.TabIndex = 1;
-            this.addFormTemplate.SelectedIndexChanged += new System.EventHandler(this.addFormTemplate_SelectedIndexChanged);
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Content});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(813, 413);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // saveButton
             // 
+            this.saveButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.saveButton.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.saveButton.FlatAppearance.BorderSize = 0;
             this.saveButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.saveButton.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold);
             this.saveButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.saveButton.Location = new System.Drawing.Point(657, 373);
+            this.saveButton.Location = new System.Drawing.Point(653, 5);
             this.saveButton.Margin = new System.Windows.Forms.Padding(2);
             this.saveButton.Name = "saveButton";
             this.saveButton.Size = new System.Drawing.Size(148, 34);
@@ -69,30 +69,58 @@
             this.saveButton.Text = "Save";
             this.saveButton.UseVisualStyleBackColor = false;
             // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(813, 413);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.saveButton);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 363);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(813, 50);
+            this.panel2.TabIndex = 4;
+            // 
+            // Content
+            // 
+            this.Content.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Content.FillWeight = 200F;
+            this.Content.HeaderText = "Content";
+            this.Content.Name = "Content";
+            this.Content.ReadOnly = true;
+            // 
             // AddList
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(813, 417);
-            this.Controls.Add(this.saveButton);
+            this.ClientSize = new System.Drawing.Size(813, 413);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Margin = new System.Windows.Forms.Padding(2);
-            this.MaximizeBox = false;
             this.Name = "AddList";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Add Paragraphs";
             this.Load += new System.EventHandler(this.AddList_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button saveButton;
-        private System.Windows.Forms.CheckedListBox addFormTemplate;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Content;
     }
 }
--- AddList.cs
+++ AddList.cs
@@ -17,12 +17,146 @@ namespace LetterOfOffer
     {
         public string SelectedTableName { get; set; }
 
+
+
+        /*        public AddList()
+                {
+                    InitializeComponent();
+
+                    // Create a dictionary to associate the preview text with the corresponding database values
+                    Dictionary<string, Tuple<int, string>> previewTextToParagraphMap = new Dictionary<string, Tuple<int, string>>();
+
+                    try
+                    {
+                        // Load data from the SQLite database
+                        string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "LetterOfOffer", "MyDatabase.sqlite");
+
+                        // Ensure the directory exists
+                        Directory.CreateDirectory(Path.GetDirectoryName(dbPath));
+
+                        // Use the dbPath variable when creating your SQLite connection
+                        string connectionString = "Data Source=" + dbPath + ";Version=3;";
+
+                        using (var connection = new SQLiteConnection(connectionString))
+                        {
+                            connection.Open();
+                            string sql = "SELECT * FROM paragraphs";
+                            using (var command = new SQLiteCommand(sql, connection))
+                            {
+                                using (var reader = command.ExecuteReader())
+                                {
+                                    while (reader.Read())
+                                    {
+                                        if (reader["content"] != DBNull.Value)
+                                        {
+                                            int id = Convert.ToInt32(reader["id"]);
+                                            string content = reader["content"].ToString();
+
+                                            // Convert RTF content to plain text
+                                            string plainContent = string.Empty;
+                                            using (RichTextBox rtb = new RichTextBox())
+                                            {
+                                                try
+                                                {
+                                                    rtb.Rtf = content;
+                                                    plainContent = rtb.Text;
+                                                }
+                                                catch (ArgumentException)
+                                                {
+                                                    // If the content is not valid RTF, then just treat it as plain text
+                                                    plainContent = content;
+                                                }
+                                            }
+
+                                            previewTextToParagraphMap[$"Paragraph {previewTextToParagraphMap.Count + 1}"] = new Tuple<int, string>(id, plainContent); // Mapping the actual database value for the preview text
+                                            addFormTemplate.Items.Add($"{id} - {plainContent}", false); // Adding items to the list box, default unchecked
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        // Log or display the exception as needed
+                        Console.WriteLine(ex.ToString());
+                    }
+
+
+                    // Handle saveButton Click event
+                    saveButton.Click += (s, args) =>
+                    {
+                        try
+                        {
+                            string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "LetterOfOffer", "MyDatabase.sqlite");
+
+                            // Ensure the directory exists
+                            Directory.CreateDirectory(Path.GetDirectoryName(dbPath));
+
+                            // Use the dbPath variable when creating your SQLite connection
+                            string connectionString = "Data Source=" + dbPath + ";Version=3;";
+
+                            using (var connection = new SQLiteConnection(connectionString))
+                            {
+                                connection.Open();
+
+                                // Iterate through each item in the list box
+                                for (int i = 0; i < addFormTemplate.Items.Count; i++)
+                                {
+                                    // Check if the item is checked
+                                    if (addFormTemplate.GetItemChecked(i))
+                                    {
+                                        // Get the item's preview text
+                                        string previewText = addFormTemplate.Items[i].ToString();
+
+                                        // Attempt to parse the ID from the preview text
+                                        if (int.TryParse(previewText.Split(' ')[0], out int id))
+                                        {
+                                            // If successful, retrieve the corresponding database value from the dictionary using the ID
+                                            var matchingEntry = previewTextToParagraphMap.First(entry => entry.Value.Item1 == id);
+
+                                            // Insert the paragraph ID and itemOrder into the selected table
+                                            string sql = $"INSERT INTO \"{SelectedTableName}\" (paragraph_id, itemOrder) VALUES (@ParagraphId, @ItemOrder)";
+                                            using (var command = new SQLiteCommand(sql, connection))
+                                            {
+                                                command.Parameters.AddWithValue("@ParagraphId", matchingEntry.Value.Item1);
+                                                command.Parameters.AddWithValue("@ItemOrder", matchingEntry.Value.Item1);  // itemOrder is now the same as ParagraphId
+                                                command.ExecuteNonQuery();
+                                            }
+                                        }
+                                        else
+                                        {
+                                            // If parsing failed, show an error message (or handle the error appropriately)
+                                            MessageBox.Show("There was an error parsing the ID from the preview text. Please check the preview text format.");
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            // Log or display the exception as needed
+                            Console.WriteLine(ex.ToString());
+                        }
+
+                        MessageBox.Show("Checked items were saved to the database.");
+                    };
+                }*/
+
+
         public AddList()
         {
             InitializeComponent();
 
             // Create a dictionary to associate the preview text with the corresponding database values
             Dictionary<string, Tuple<int, string>> previewTextToParagraphMap = new Dictionary<string, Tuple<int, string>>();
+
+            DataGridViewCheckBoxColumn checkBoxColumn = new DataGridViewCheckBoxColumn();
+            checkBoxColumn.ValueType = typeof(bool);
+            checkBoxColumn.Name = "checkBoxColumn";
+            checkBoxColumn.HeaderText = "Select";
+            dataGridView1.Columns.Insert(0, checkBoxColumn);
+
 
             try
             {
@@ -66,8 +200,8 @@ namespace LetterOfOffer
                                         }
                                     }
 
-                                    previewTextToParagraphMap[$"Paragraph {previewTextToParagraphMap.Count + 1}"] = new Tuple<int, string>(id, plainContent); // Mapping the actual database value for the preview text
-                                    addFormTemplate.Items.Add($"{id} - {plainContent}", false); // Adding items to the list box, default unchecked
+                                    previewTextToParagraphMap[$"Paragraph {previewTextToParagraphMap.Count + 1}"] = new Tuple<int, string>(id, content); // Mapping the actual database value for the preview text
+                                    dataGridView1.Rows.Add(false, $"{id} - {plainContent}"); // Adding items to the DataGridView, default unchecked
                                 }
                             }
                         }
@@ -79,7 +213,6 @@ namespace LetterOfOffer
                 // Log or display the exception as needed
                 Console.WriteLine(ex.ToString());
             }
-
 
             // Handle saveButton Click event
             saveButton.Click += (s, args) =>
@@ -93,69 +226,53 @@ namespace LetterOfOffer
 
                     // Use the dbPath variable when creating your SQLite connection
                     string connectionString = "Data Source=" + dbPath + ";Version=3;";
-
                     using (var connection = new SQLiteConnection(connectionString))
                     {
                         connection.Open();
 
-                        // Iterate through each item in the list box
-                        for (int i = 0; i < addFormTemplate.Items.Count; i++)
+                        // Iterate through each row in the DataGridView
+                        foreach (DataGridViewRow row in dataGridView1.Rows)
                         {
-                            // Check if the item is checked
-                            if (addFormTemplate.GetItemChecked(i))
+                            // Avoid operating on empty rows (which are null)
+                            if (row.Cells[0].Value != null && row.Cells[1].Value != null)
                             {
-                                // Get the item's preview text
-                                string previewText = addFormTemplate.Items[i].ToString();
-
-                                // Attempt to parse the ID from the preview text
-                                if (int.TryParse(previewText.Split(' ')[0], out int id))
+                                // Check if the checkbox is checked
+                                bool isChecked = (bool)row.Cells[0].Value;
+                                if (isChecked)
                                 {
-                                    // If successful, retrieve the corresponding database value from the dictionary using the ID
-                                    var matchingEntry = previewTextToParagraphMap.First(entry => entry.Value.Item1 == id);
+                                    // Get the paragraph ID from the row's data
+                                    int id = Convert.ToInt32(row.Cells[1].Value.ToString().Split('-')[0].Trim());
 
                                     // Insert the paragraph ID and itemOrder into the selected table
                                     string sql = $"INSERT INTO \"{SelectedTableName}\" (paragraph_id, itemOrder) VALUES (@ParagraphId, @ItemOrder)";
                                     using (var command = new SQLiteCommand(sql, connection))
                                     {
-                                        command.Parameters.AddWithValue("@ParagraphId", matchingEntry.Value.Item1);
-                                        command.Parameters.AddWithValue("@ItemOrder", matchingEntry.Value.Item1);  // itemOrder is now the same as ParagraphId
+                                        command.Parameters.AddWithValue("@ParagraphId", id);
+                                        command.Parameters.AddWithValue("@ItemOrder", id); // itemOrder is now the same as ParagraphId
                                         command.ExecuteNonQuery();
                                     }
-                                }
-                                else
-                                {
-                                    // If parsing failed, show an error message (or handle the error appropriately)
-                                    MessageBox.Show("There was an error parsing the ID from the preview text. Please check the preview text format.");
                                 }
                             }
                         }
                     }
+
+
+                    MessageBox.Show("Checked items were saved to the database.");
                 }
                 catch (Exception ex)
                 {
                     // Log or display the exception as needed
                     Console.WriteLine(ex.ToString());
                 }
-
-                MessageBox.Show("Checked items were saved to the database.");
             };
-
-
-
-
-
-
-
-
         }
-
 
         private void AddList_Load(object sender, EventArgs e)
         {
 
         }
 
-        private void addFormTemplate_SelectedIndexChanged(object sender, EventArgs e)
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
         }
